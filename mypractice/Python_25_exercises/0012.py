8bb31015fc560e49a9485e13204e8702576c3d7f
DodgeV <49369085+DodgeV@users.noreply.github.com>
2024-06-27 17:04:27 +0800

    Update 0012.py

--- mypractice/Python_25_exercises/0012.py
+++ mypractice/Python_25_exercises/0012.py
@@ -3,40 +3,22 @@
 例如当用户输入「北京是个好城市」，则变成「**是个好城市」。
 '''
 
-import re
-
-class Input(object):
-
-    def __init__(self):
-        self.filtered_words = list()
-        self.in_string = ''
-        self.out_string = 'Human Rights'
-        self.load_filtered_words()
-
-    def load_filtered_words(self, filename='filtered_words.txt'):
-        with open(filename, 'r', encoding='utf8') as file:
+class SensitiveWordFilter:
+    def __init__(self, filename):
+        self.sensitive_words = list()
+        with open(filename, 'r', encoding='utf-8') as file:
             for line in file.readlines():
-                self.filtered_words.append(line.strip())
+                words = line.strip().split(',')
+                self.sensitive_words += words   # 直接相加即可合并列表
 
-    def filter_words(self):
-        pattern = r'\b(' + '|'.join(re.escape(word) for word in self.filtered_words) + r')\b'  
-        self.out_string = re.sub(pattern, lambda match: '*' * len(match.group(1)), self.in_string)  
-        for word in self.filtered_words:
-            if word in self.out_string:
-                self.out_string = self.out_string.replace(word, len(word)*'*')
+    def filter_words(self, text):
+        for word in self.sensitive_words:
+            text = text.replace(word, '*' * len(word))
+        return text
 
-    def user_input(self, filename='exercise.txt'):
-        if not filename:
-            self.in_string = input('>')
-        else:
-            with open(filename, 'r', encoding='utf-8') as file:
-                self.in_string = file.read()
 
-    def std_output(self):
-        self.filter_words()
-        print(self.out_string)
-
-if __name__ == '__main__':
-    i = Input()
-    i.user_input()
-    i.std_output() 
+# 使用示例
+filter_obj = SensitiveWordFilter('filtered_words.txt')
+user_input = input("请输入一段文字：")
+filtered_text = filter_obj.filter_words(user_input)
+print(filtered_text)
