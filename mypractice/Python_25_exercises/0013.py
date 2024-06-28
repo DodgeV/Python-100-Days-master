54789ec5992d9bbf3a78e1752386d8d934fe26ec
DodgeV <49369085+DodgeV@users.noreply.github.com>
2024-06-28 21:55:55 +0800

    Update 0013.py

--- mypractice/Python_25_exercises/0013.py
+++ mypractice/Python_25_exercises/0013.py
@@ -3,43 +3,57 @@
 '''
 
 import requests
-from pyquery import PyQuery
-from urllib.request import urlopen
-
+import re
+from urllib.parse import urljoin
 
 class DownLoadImage(object):
 
     def __init__(self):
         self.urls = list()
-        self.url = 'http://tieba.baidu.com/p/2166231880'
+        self.url = 'https://tieba.baidu.com/p/1710123825'
         self.headers = {
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-            'Accept-Encoding': 'gzip, deflate',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) Chrome/59.0.3071.109 Safari/537.36'
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'Accept-Encoding': 'gzip, deflate, br, zstd',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36'
         }
         self.s = requests.session()
         self.s.headers.update(self.headers)
 
     def get_image_url(self):
         resp = self.s.get(self.url)
-        doc = PyQuery(resp.content.decode())
-        imgs = doc.find('img.BDE_Image')
-        for img in imgs.items():
-            self.urls.append(img.attr('src'))
+        resp.raise_for_status()
+        html_content = resp.text
+        img_regex = r'<img[^>]+class="BDE_Image"[^>]+src="([^"]+)"'
+        matches = re.findall(img_regex, html_content)
+        for match in matches:
+            full_url = urljoin(self.url, match)
+            self.urls.append(full_url)
 
     def save(self):
-        for i in range(len(self.urls)):
-            url = self.urls[i]
-            print(url)
-            resp = self.s.get(url)
-            filename = 'img' + str(i) + '.jpg'
-            with open(filename, 'wb') as file:
-                file.write(resp.content)
+        for i, url in enumerate(self.urls):
+            print('Downloading IMG -->',url)
+            try:
+                resp = self.s.get(url, stream=True)  # 对于大文件，使用stream=True来分块读取
+                resp.raise_for_status()  # 确保请求成功
+                # 尝试从Content-Type确定文件扩展名
+                content_type = resp.headers.get('Content-Type', '')
+                if 'image/jpeg' in content_type:
+                    ext = '.jpg'
+                elif 'image/png' in content_type:
+                    ext = '.png'
+                else:
+                    ext = '.bin'  # 默认为二进制文件
+                filename = f'img{i}{ext}'
+                with open(filename, 'wb') as file:
+                    for chunk in resp.iter_content(1024): # 分块写入文件
+                        file.write(chunk)
+            except requests.RequestException as e:
+                print(f"Error downloading {url}: {e}")
 
     def download(self):
         self.get_image_url()
         self.save()
 
-if __name__ == '__main__':
-    d = DownLoadImage()
-    d.download()
+
+d = DownLoadImage()
+d.download()
